7271ddb34752c1241b9091767272c1651f7a7037
Cyr Percpernot <kcpernot@yahoo.co.jp>
2026-01-03 17:37:57 +0900

    Fix: move vercel.json to frontend and simplify paths for SPA routing

--- frontend/static/js/config.js
+++ frontend/static/js/config.js
@@ -4,6 +4,6 @@ const isLocal = window.location.hostname === "localhost" || window.location.host
 // On attache la variable à "window" pour qu'elle soit globale
 window.API_BASE_URL = isLocal 
     ? "http://127.0.0.1:8000"  // Ton backend local Python
-    : "https://ton-backend-sur-render.onrender.com"; // Ton futur backend Render
+    : "https://kanjilock-backend.onrender.com"; // Ton futur backend Render
 
 console.log("Mode:", isLocal ? "Local" : "Production", "| API:", window.API_BASE_URL);
